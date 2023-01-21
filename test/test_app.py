2c5fdd975a437a27dfdcd34ef0d055707397cccb
Ali Bahramali <ma.bahramali@gmail.com>
2023-01-21 14:39:29 +0100

    add mocker tedst

--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,8 @@
+from flask import Flask, Blueprint
+import psutil
+from docker import app
 import pytest
 from flask import Flask
-from flask import Blueprint
 import psutil
 from app import app, bp, bptemp, bpdisk
 from flask.testing import FlaskClient
@@ -42,4 +44,15 @@ def test_integration(client):
     assert response.status_code == 200
     assert response.data.decode().startswith("Disk Usage ist")
 
+
+def test_auslastung(mocker):
+    # Use the mocker fixture to patch the psutil.cpu_percent function
+    mocker.patch("psutil.cpu_percent", return_value=50)
+
+    # Use the client fixture to make a GET request to the '/cpu/auslastung' endpoint
+    response = app.test_client().get('/cpu/auslastung')
+
+    # Assert that the response has a 200 status code and the expected response data
+    assert response.status_code == 200
+    assert response.data.decode() == "CPU Auslastung ist 50%"
 
