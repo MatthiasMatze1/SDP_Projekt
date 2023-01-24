6ac4ada6fa91050df0609a715a214af3d8b815d3
Pranjal Khan <pranjal.khan@hotmail.com>
2023-01-24 21:49:52 +0000

    Flake8 added

--- docker/test_app.py
+++ docker/test_app.py
@@ -1,14 +1,15 @@
 import pytest
 from app import app
 import re
-from psutil._common import sdiskusage
 import psutil
+
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     client = app.test_client()
     yield client
+
 
 @pytest.mark.unit
 def test_cpu_auslastung(client):
@@ -32,11 +33,13 @@ def test_cpu_temp_error(client):
     assert response.status_code == 200
     assert response.data.decode().startswith("Temperatur ")
 
+
 @pytest.mark.unit
 def test_disk_usage(client):
     response = client.get('/disk/usage')
     assert response.status_code == 200
     assert response.data.decode().startswith("Disk ")
+
 
 @pytest.mark.integration
 def test_integration(client):
@@ -54,6 +57,7 @@ def test_integration(client):
     assert response.status_code == 200
     assert response.data.decode().startswith("Disk Usage ist")
 
+
 def test_auslastung(mocker):
     # Use the mocker fixture to patch the psutil.cpu_percent function
     mocker.patch("psutil.cpu_percent", return_value=50)
@@ -66,15 +70,18 @@ def test_auslastung(mocker):
     assert response.status_code == 200
     assert response.data.decode() == "CPU Auslastung ist 50%"
 
+
 def test_lowcpu(mocker):
     mocker.patch("psutil.cpu_percent", return_value=10)
     response = app.test_client().get('/cpu/auslastung')
     assert response.status_code == 200
     assert response.data.decode() == "CPU Auslastung ist 10%"
 
+
 def test_disk(mocker):
     # Use the mocker fixture to patch the psutil.disk_usage function
-    mocker.patch("psutil.disk_usage", return_value=psutil._common.sdiskusage(total=1000, used=500, free=500, percent=50))
+    mocker.patch("psutil.disk_usage", return_value=psutil._common.sdiskusage
+            (total=1000, used=500, free=500, percent=50))
 
     # Use the client fixture to make a GET request to the '/disk/usage' endpoint
     response = app.test_client().get('/disk/usage')
